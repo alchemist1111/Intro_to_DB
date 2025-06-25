87fa296aa6c3f5a3929dda547d13b1b962063ef2
alchemist1111 <emoyoeugine@gmail.com>
2025-06-25 05:07:26 +0300

    added sql command to insert single row to customers table

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+-- USE the alx_book_store database
+USE alx_book_store;
+
+-- INSERT a single row into the 'customers' table
+INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
