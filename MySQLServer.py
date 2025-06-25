c82d43ee593217c8ae1eaca6803c26c39e410a0a
alchemist1111 <emoyoeugine@gmail.com>
2025-06-25 04:39:51 +0300

    corrected error handling

--- MySQLServer.py
+++ MySQLServer.py
@@ -18,7 +18,7 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
             cursor.close() # Close the cursor
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error: {e}")
     finally:
         if connection.is_connected():
