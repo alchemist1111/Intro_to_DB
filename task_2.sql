16ad307c611f576e195a4bde8fbcdd54d304ba92
alchemist1111 <emoyoeugine@gmail.com>
2025-06-25 04:50:19 +0300

    used and created the database tables

---
+++ task_2.sql
@@ -0,0 +1,44 @@
+-- Use the newly created database
+USE alx_book_store;
+
+-- Create the Authors table
+CREATE TABLE Authors (COMMENT
+     author_id INT AUTO_INCREMENT PRIMARY KEY,
+     author_name VARCHAR(215) NOT NULL,
+);
+
+-- Create the Books table
+CREATE TABLE Books (COMMENT
+     book_id INT AUTOINCREMENT PRIMARY KEY,
+     title VARCHAR(130),
+     author_id INT,
+     price DOUBLE,
+     publication_date DATE,
+     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- Create the Customers table
+CREATE TABLE Customers (COMMENT
+      customer_id INT AUTOINCREMENT PRIMARY KEY,
+      customer_name VARCHAR(215),
+      email VARCHAR(215),
+      address TEXT
+);
+
+-- Create the Orders table
+CREATE TABLE Orders (COMMENT
+      order_id INT AUTOINCREMENT PRIMARY KEY,
+      customer_id INT,
+      order_date DATE,
+      FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- Create the Order_details table
+CREATE TABLE Order_Details (COMMENT
+      orderdetailid INT AUTOINCREMENT PRIMARY KEY,
+      order_id INT,
+      book_id INT,
+      quantity DOUBLE,
+      FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+      FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
